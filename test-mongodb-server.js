e4a5b47227287dd5e4d221ae4f7039e71b4ef855
Mokhaled2004 <mokhaled2004@gmail.com>
2024-12-07 21:54:50 +0200

    Updated

--- test-mongodb-server.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect(
-    'mongodb+srv://<username>:<password>@<cluster>.mongodb.net/<dbname>?retryWrites=true&w=majority',
-    { useNewUrlParser: true, useUnifiedTopology: true }
-);
